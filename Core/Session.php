2e990adb788b16711975424a6396eb836f1a2ce2
nastasagr <alexanstgr@gmail.com>
2026-03-15 16:07:33 +0200

    convert to static methods

--- Core/Session.php
+++ Core/Session.php
@@ -4,45 +4,36 @@ namespace Luna\Core;
 
 class Session
 {
-    public static function start(): void
+    public function __construct()
     {
         if (session_status() === PHP_SESSION_NONE) {
             session_start();
         }
     }
 
-
-    public static function set(string $key, mixed $value): void
+    public function set(string $key, mixed $value): void
     {
-        self::start();
         $_SESSION[$key] = $value;
     }
 
-    public static function get(string $key, mixed $default = null): mixed
+    public function get(string $key, mixed $default = null): mixed
     {
-        self::start();
         return $_SESSION[$key] ?? $default;
     }
 
-
-    public static function has(string $key): bool
+    public function has(string $key): bool
     {
-        self::start();
-        return isset($_SESSION[$key]);
+        return array_key_exists($key, $_SESSION);
     }
 
-
-    public static function remove(string $key): void
+    public function remove(string $key): void
     {
-        self::start();
         unset($_SESSION[$key]);
     }
 
-    public static function destroy(): void
+    public function destroy(): void
     {
-        self::start();
         session_unset();
         session_destroy();
     }
-
 }
